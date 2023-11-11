87ad2cd9aa322f5e6cdc416fa36b7c58a9c2d7c3
ChinoManda <francofedullo22@gmail.com>
2023-11-10 21:42:37 -0300

    Index with list options

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,9 @@
+<h2>Users</h2>
+
+<!-- Menú de navegación específico para la sección de posts -->
+<nav>
+    <ul>
+        <li><a href="{{ route('users.userslist') }}">Listar Todos los Usuarios</a></li>
+        <li><a href="{{ route('users.user') }}">Mostrar Usuario por ID</a></li>
+    </ul>
+</nav>
